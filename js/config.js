a43f16e7485de2d1db0c30e4b6ad84b3e0080098
osboxes.org <osboxes@osboxes>
2017-10-10 23:43:12 +0100

    A

---
+++ js/config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Peter Ryszkiewicz (https://github.com/pRizz) on 9/22/2017.
+ * https://github.com/pRizz/Monero-Donator
+ */
+
+const config = {
+    publicCoinHiveKey: 'zXSF784LiJwOBzp4snn01FBDV0nMNXcc',
+    baseURL: 'https://himajara.github.io/'
+}
